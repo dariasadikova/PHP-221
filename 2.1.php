afb0a9de2a5851525043c7aefddd7873ec31b6f8
Dasha <sadidash@yandex.ru>
2023-02-22 09:15:38 +0300

    1lab

---
+++ 2.1.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>1 lab</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <?php
+            echo "<img src='logo-mospolytech.png'>";
+        ?>
+        <h1>
+            <?php
+                echo "2.1.Домашняя работа: Hello, World!";
+            ?>
+        </h1>
+    </header>
+    <main>      
+        <div>
+            <?php
+                echo "Hello, World!";
+            ?>
+        </div> 
+    </main>
+    <footer>
+        <p>
+            <?php
+                echo "Московский Политех, веб-технологии";
+            ?>
+        </p>
+    </footer>
+
+</body>
+</html>
